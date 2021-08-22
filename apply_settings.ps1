732b3617c229d794503ca4c38da9f2de32c792f6
Jax <75493600+JaxRaffnix@users.noreply.github.com>
2021-08-22 18:13:25 +0200

    fixed script calling

--- apply_settings.ps1
+++ apply_settings.ps1
@@ -1,3 +1,5 @@
 # Copies settings.json to Windows Terminal location
 
+$PSScriptRoot | Set-Location
+
 Copy-Item .\settings.json -Destination $HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
