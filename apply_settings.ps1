2a08dac0e5985740b4692dabb80bfe2497316292
Jax Raffnix <75493600+JaxRaffnix@users.noreply.github.com>
2022-08-06 01:02:31 +0200

    fixed path

--- apply_settings.ps1
+++ apply_settings.ps1
@@ -2,6 +2,6 @@
 
 $PSScriptRoot | Set-Location
 
-Copy-Item .\settings.json -Destination $HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
+Copy-Item .\settings.json -Destination "$HOME\AppData\Local\Microsoft\Windows Terminal"
 
 # Test Words.
